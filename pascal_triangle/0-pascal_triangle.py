17b690ade4bb86fe2504db682af4c384e026f183
Guillaume Vaudet <guillaumevaudet.pro@gmail.com>
2026-05-17 21:20:45 +0200

    Remove if guard

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -24,14 +24,3 @@ def pascal_triangle(n):
         pascal_list.append(new_list)
 
     return pascal_list
-
-
-if __name__ == "__main__":
-    def print_triangle(triangle):
-        """
-        Print the triangle
-        """
-        for row in triangle:
-            print("[{}]".format(",".join([str(x) for x in row])))
-
-    print_triangle(pascal_triangle(5))
