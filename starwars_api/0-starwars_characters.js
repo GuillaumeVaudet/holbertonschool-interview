10d018e322080a7117dbacf6e5ce667b5c7b8f58
Guillaume Vaudet <guillaumevaudet.pro@gmail.com>
2026-06-27 00:23:48 +0200

    Add algo for starwars api

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -1,5 +1,33 @@
 #!/usr/bin/env node
 // #!/usr/bin/node
 
+const request = require('request');
+const MOVIE_ID = Number(process.argv[2]);
+const API_URL = `https://swapi-api.hbtn.io/api/films/${MOVIE_ID}`;
 
-console.log(process.argv);
+if (isNaN(MOVIE_ID)) {
+  console.log('Le movie id n\'est pas correct');
+  process.exit(1);
+}
+
+request(API_URL, function (err, response, body) {
+  if (err) return console.error(err);
+
+  const movieData = JSON.parse(body);
+  const charactersList = movieData.characters;
+
+  function printCharacter (index) {
+    if (index >= charactersList.length) return;
+
+    request(charactersList[index], function (err, response, body) {
+      if (err) return console.error(err);
+
+      const charactersData = JSON.parse(body);
+      console.log(charactersData.name);
+
+      printCharacter(index + 1);
+    });
+  }
+
+  printCharacter(0);
+});
